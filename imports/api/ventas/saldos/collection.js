ef44a85b241936e11462425065a115e3a02c2a3b
Juan Valentin <j.vlt.mtz@gmail.com>
2017-09-19 12:17:16 -0500

    Se crea coleccion VentasSaldos, metodos y publicaciones

---
+++ imports/api/ventas/saldos/collection.js
@@ -0,0 +1,50 @@
+/**
+ * Created by jvltmtz on 15/09/17.
+ */
+import {Mongo} from "meteor/mongo";
+import ventasSaldosCounts from "./saldosCount";
+
+class VentasSaldosCollection extends Mongo.Collection {
+    insert(doc, callback) {
+        const result = super.insert(doc, callback);
+        return result;
+    }
+
+    update(selector, modifier, options, callback) {
+        const result = super.update(selector, modifier, options, callback);
+        if (_.has(modifier.$set, 'saldoCobrar')) {
+            ventasSaldosCounts._afterUpdateSaldos(selector);
+        }
+        return result;
+    }
+}
+
+export const VentasSaldos = new VentasSaldosCollection('ventasSaldos');
+
+VentasSaldos.deny({
+    insert() {
+        return true;
+    },
+    update() {
+        return true;
+    },
+    remove() {
+        return true;
+    }
+});
+
+const Schema = {};
+
+Schema.ventasSaldos = new SimpleSchema({
+    _id: {type: String, regEx: SimpleSchema.RegEx.Id},
+    ventaOrdenId: {type: String, regEx: SimpleSchema.RegEx.Id},
+    tiendaId: {type: String, regEx: SimpleSchema.RegEx.Id},
+    total: {type: Number, decimal: true, defaultValue: 0.0},
+    subTotal: {type: Number, decimal: true, defaultValue: 0.0},
+    saldoCobrar: {type: Number, decimal: true, defaultValue: 0.0},
+    totalPagos: {type: Number, decimal: true, defaultValue: 0.0},
+    fechaCreacion: {type: Date, defaultValue: new Date},
+    iva: {type: Number, optional: true}
+});
+
+VentasSaldos.attachSchema(Schema.ventasSaldos);
---
+++ imports/api/ventas/saldos/server/publish.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jvltmtz on 18/09/17.
+ */
+
+import {VentasSaldos} from "../collection";
+
+if (Meteor.isServer) {
+
+    Meteor.publish('ventasSaldos.ventaOrdenId', function (filter) {
+        if (Object.keys(filter).length === 0 && filter.constructor === Object) {
+            this.ready();
+        } else {
+            return VentasSaldos.find(filter, {fields: {fechaCreacion: 0}});
+        }
+    });
+
+}
