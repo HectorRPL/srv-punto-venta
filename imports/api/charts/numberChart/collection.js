2b66fe056a3e0ce52a6116fe227e087f88152396
hEctor <hector.kaizen@gmail.com>
2017-10-18 09:40:32 -0500

    Se crea el esqueleto para los charts

---
+++ imports/api/charts/numberChart/collection.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Héctor on 18/10/2017.
+ */
+// debe tener 6 propiedades del tipo number
---
+++ imports/api/charts/numberChart/server/publish.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Héctor on 18/10/2017.
+ */
+// Deberá publicar las propiedades
