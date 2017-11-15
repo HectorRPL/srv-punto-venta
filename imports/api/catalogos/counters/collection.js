0d974a803bc3f15346d3ffa5b6e4b4f688508dc2
Juan Valentin <j.vlt.mtz@gmail.com>
2017-11-15 17:12:57 -0600

    Se eliminan counter inecesarios y se agrega schema definitivo.

--- imports/api/catalogos/counters/collection.js
+++ imports/api/catalogos/counters/collection.js
@@ -9,3 +9,26 @@ Counters.deny({
     update() {return true;},
     remove() {return true;}
 });
+
+
+const Schema = {};
+
+Schema.counters = new SimpleSchema({
+    _id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    tiendaId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    nombre: {
+        type: String
+    },
+    seq: {
+        type: Number,
+        defaultValue: 0
+    }
+});
+
+Counters.attachSchema(Schema.counters);
