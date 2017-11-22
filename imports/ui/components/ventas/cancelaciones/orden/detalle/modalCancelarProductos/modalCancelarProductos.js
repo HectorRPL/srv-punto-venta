f171f7ed7e34dcc546f289a68de943b5f25d916f
Juan Valentin <j.vlt.mtz@gmail.com>
2017-11-22 15:56:20 -0600

    se importan metodos para las cancelaciones.

--- imports/ui/components/ventas/cancelaciones/orden/detalle/modalCancelarProductos/modalCancelarProductos.js
+++ imports/ui/components/ventas/cancelaciones/orden/detalle/modalCancelarProductos/modalCancelarProductos.js
@@ -1,12 +1,12 @@
 /**
  * Created by jvltmtz on 15/11/2017.
  */
-import {Session}                from "meteor/session";
-import {VentasCancelaciones}   from "../../../../../../../api/ventas/cancelaciones/collection";
-//import {crearCompraCancelacion} from "../../../../../../api/ventas/cancelaciones/methods";
-import {name as Alertas}        from "../../../../../comun/alertas/alertas";
+import {Session} from "meteor/session";
+import {VentasCancelaciones} from "../../../../../../../api/ventas/cancelaciones/collection";
+import {crearVentaCancelacion} from "../../../../../../../api/ventas/cancelaciones/methods";
+import {name as Alertas} from "../../../../../comun/alertas/alertas";
 import {name as MotivoCancelar} from "../../../../../comun/selects/elegirMotivoCancelar/elegirMotivoCancelar";
-import template                 from "./modalCancelarProductos.html";
+import template from "./modalCancelarProductos.html";
 
 class ModalCancelarProductos {
     constructor($scope, $reactive, $stateParams) {
@@ -15,9 +15,10 @@ class ModalCancelarProductos {
         this.scope = $scope;
 
         this.tiendaId = Session.get('estacionTrabajoId');
-        this.compraOrdenId = $stateParams.compraOrdenId;
+        this.ventaOrdenId = $stateParams.ventaOrdenId;
+        this.motivo = '';
 
-        this.subscribe('comprasCancelaciones.lista', () => [
+        this.subscribe('ventasCancelaciones.lista', () => [
             {
                 partidaId: this.getReactively('resolve.partida._id')
             }
@@ -30,14 +31,18 @@ class ModalCancelarProductos {
             }
         });
     }
+
     aceptar() {
-        let entrega = {
+        console.log(this.motivo);
+        let cancelacion = {
             partidaId: this.resolve.partida._id,
             tiendaId: this.tiendaId,
-            compraOrdenId: this.compraOrdenId,
-            numProductos: this.numProductosCancelados
+            ventaOrdenId: this.ventaOrdenId,
+            numProductos: this.numProductosCancelados,
+            motivo: '1'
         };
-        crearCompraCancelacion.callPromise(entrega).then(this.$bindToContext((result) => {
+        console.log(cancelacion);
+        crearVentaCancelacion.callPromise(cancelacion).then(this.$bindToContext((result) => {
             this.tipoMsj = 'success';
             this.limpiarForma();
         })).catch(this.$bindToContext((err) => {
@@ -46,10 +51,12 @@ class ModalCancelarProductos {
             this.tipoMsj = 'danger';
         }));
     }
+
     limpiarForma() {
         this.monto = '';
         this.scope.modalCancelarProductosFrm.$setPristine();
     }
+
     cerrar() {
         this.modalInstance.dismiss('Cancelado');
     }
